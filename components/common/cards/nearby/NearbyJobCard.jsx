b337151b1eba7d86be047a0205cf59513f63aec4
martaeterlast <142797973+martaeterlast@users.noreply.github.com>
2024-01-08 15:31:25 +0100

    created navigation to other page

--- components/common/cards/nearby/NearbyJobCard.jsx
+++ components/common/cards/nearby/NearbyJobCard.jsx
@@ -6,7 +6,7 @@ import styles from './nearbyjobcard.style'
 
 const NearbyJobCard = ({ job, handleCardPress }) => {
   return (
-    <TouchableOpacity style={styles.container} onPress={() => handleCardPress(item)}>
+    <TouchableOpacity style={styles.container} onPress={() => handleCardPress(job)}>
       <TouchableOpacity style={styles.logoContainer}>
         <Image source={{ uri: checkImageURL(job.employer_logo) ? job.employer_logo : 'https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg'}} style={styles.logoImage} resizeMode='contain' />
       </TouchableOpacity>
